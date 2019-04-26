f5d4a90f571a6ea59ac8e15d520324522dbf8d59
rsteinkopfjr <rob.steinkopf@gmail.com>
2019-04-26 13:14:46 -0400

    Updated mongoose connect

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.static("public"));
 // Connect to the Mongo DB
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/fantasydb";
 
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
 
